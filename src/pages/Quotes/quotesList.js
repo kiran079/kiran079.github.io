014766c23a679094097fb6fc7ee0437d81bcd28d
kiran079 <kiran79bh@gmail.com>
2025-02-24 19:52:49 -0600

    more quotes from movies

--- src/pages/Quotes/quotesList.js
+++ src/pages/Quotes/quotesList.js
@@ -4,5 +4,7 @@ export const QUOTES_LIST = [
 	'A man can do as he will, but not will as he will.',
 	"If everybody is in on it... I'd have to be in on it too.",
 	"Well, if you can't tell, does it matter?",
-	"I do not play this instrument as well as I should wish to, but I have always supposed that to be my own fault, because I would not take the trouble of practicing."
+	'I do not play this instrument as well as I should wish to, but I have always supposed that to be my own fault, because I would not take the trouble of practicing.',
+	"... live every day as if I've deliberately come back to this one day, to enjoy it as if it was the full final day of my extraordinary ordinary life.",
+	"What if this is a past life as well, and we are already something to each other in our next life? Who do you think we are then? ... See you then.",
 ];
