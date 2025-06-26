9b242d4504963557fc63c79b694914a665836265
kiran079 <kiran79bh@gmail.com>
2025-06-25 22:01:58 -0500

    Added quotes from books

--- src/pages/Quotes/quotesList.js
+++ src/pages/Quotes/quotesList.js
@@ -8,5 +8,7 @@ export const QUOTES_LIST = [
 	"... live every day as if I've deliberately come back to this one day, to enjoy it as if it was the full final day of my extraordinary ordinary life.",
 	"What if this is a past life as well, and we are already something to each other in our next life? Who do you think we are then? ... See you then.",
 	"Any normal girl would call the number, meet him, return the album and see if her dream is viable. It's called a reality check. The last thing Amélie wants.",
-	"They say you need countless lifetimes of fate to meet even once in this life. If you miss it when it brushes past, that's the end."
+	"They say you need countless lifetimes of fate to meet even once in this life. If you miss it when it brushes past, that's the end.",
+	"I have been living in this country nine years longer than you have. Do you realize that?",
+	"Always resignation and acceptance. Always prudence and honour and duty. Elinor, where is your heart?"
 ];
